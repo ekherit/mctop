3171af0c1468e67d66c7ac3c3525379acf913d17
Ivan Nikolaev <i.b.nikolaev@gmail.com>
2015-12-10 11:49:37 +0600

    mctopo

--- mctopo.cpp
+++ mctopo.cpp
@@ -12,6 +12,7 @@
 
 //#include "McTopo.h"
 #include "RootMCTopo.h"
+#include "mctopo.h"
 
 
 #include "decay_topology.h"
@@ -62,29 +63,29 @@ std::string format(std::string s, int width=0)
   return s+std::string(n,' ');
 }
 
-inline decay_topology_t make_topology(const RootMCTopo  & m)
-{
-  decay_topology_t top;
-  //fill the topology
-  for(int i=0;i<m.indexmc; i++)
-  {
-    for(int j=0;j<m.indexmc;j++)
-    {
-      if(m.idx[i] == m.motheridx[j])
-      {
-        vertex_t parent(i);
-        vertex_t child(j);
-        boost::add_edge(parent,child,top);
-        top[parent].name = PdgTable[top[parent].pdgid];
-        top[parent].pdgid = m.pdgid[parent];
-        top[child].pdgid = m.pdgid[child];
-        top[child].name = PdgTable[top[child].pdgid];
-      }
-    }
-  }
-  add_hash(top);
-  return top;
-}
+//inline decay_topology_t make_topology(const RootMCTopo  & m)
+//{
+//  decay_topology_t top;
+//  //fill the topology
+//  for(int i=0;i<m.indexmc; i++)
+//  {
+//    for(int j=0;j<m.indexmc;j++)
+//    {
+//      if(m.idx[i] == m.motheridx[j])
+//      {
+//        vertex_t parent(i);
+//        vertex_t child(j);
+//        boost::add_edge(parent,child,top);
+//        top[parent].name = PdgTable[top[parent].pdgid];
+//        top[parent].pdgid = m.pdgid[parent];
+//        top[child].pdgid = m.pdgid[child];
+//        top[child].name = PdgTable[top[child].pdgid];
+//      }
+//    }
+//  }
+//  add_hash(top);
+//  return top;
+//}
 
 std::map<decay_topology_t, Long64_t> count_topologies(RootMCTopo * mctopo , unsigned long long  N, int opt)
 {
--- mctopo.h
+++ mctopo.h
@@ -4,3 +4,42 @@
 #include "RootMCTopo.h"
 
 
+
+
+
+
+inline decay_topology_t make_topology(const RootMCTopo  & m)
+{
+  decay_topology_t top;
+  //fill the topology
+  for(int i=0;i<m.indexmc; i++)
+  {
+    for(int j=0;j<m.indexmc;j++)
+    {
+      if(m.idx[i] == m.motheridx[j])
+      {
+        vertex_t parent(i);
+        vertex_t child(j);
+        boost::add_edge(parent,child,top);
+        top[parent].name = PdgTable[top[parent].pdgid];
+        top[parent].pdgid = m.pdgid[parent];
+        top[child].pdgid = m.pdgid[child];
+        top[child].name = PdgTable[top[child].pdgid];
+      }
+    }
+  }
+  add_hash(top);
+  return top;
+}
+
+inline unsigned long mctopo_hash(RootMCTopo  * m) 
+{
+  auto top = make_topology(*m);
+  return top[boost::graph_bundle].hash();
+}
+
+inline std::string mctopo_info(RootMCTopo  * m) 
+{
+  auto top = make_topology(*m);
+  return to_string(top);
+}
